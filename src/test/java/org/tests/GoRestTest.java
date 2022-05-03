db8fffd9d48936f984f10792909141dacb888703
Surajmesta <surajmesta47@gmail.com>
2022-05-03 13:27:40 +0530

    added GoRestTest post test method

--- src/test/java/org/tests/GoRestTest.java
+++ src/test/java/org/tests/GoRestTest.java
@@ -1,5 +1,6 @@
 package org.tests;
 
+import Payloadsupply.GoRestTestData;
 import Resources.BaseURI;
 import Resources.ResourceURI;
 import io.restassured.RestAssured;
@@ -7,6 +8,7 @@ import static io.restassured.RestAssured.*;
 
 import org.apache.http.HttpMessage;
 import org.apache.http.HttpResponse;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class GoRestTest {
@@ -18,6 +20,23 @@ public class GoRestTest {
 
       given().log().all().header("content-type","application/json").when().get(ResourceURI.GO_REST_RESOURCE_URL.getResourceURl())
               .then().log().all().assertThat().statusCode(200);
+    }
+
+
+//    @Test(priority = 1,dataProvider = "goresttestdata")
+//    public void goRestPostTest(String name,String gender ,String email,String status){
+//        RestAssured.baseURI = BaseURI.GO_REST_BASE_URI.getBaseURL();
+//
+//        given().log().all().header("content-type","application/json")
+//                .body(GoRestTestData.goRestPostData(name,gender,email,status))
+//                .when().post(ResourceURI.GO_REST_RESOURCE_URL.getResourceURl())
+//                .then().log().all().assertThat().statusCode(401);
+//    }
+
+
+    @DataProvider(name = "goresttestdata")
+    public String[] returnGoRestTestData(){
+        return new String[]{"Darshan","male","darshan@test.com","active"};
     }
 
 }
